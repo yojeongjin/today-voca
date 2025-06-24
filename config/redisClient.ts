c8c1b624a02559cab776e64fe8d53f57d717442a
jeongjin o <jeongjino@jeongjinui-noteubug.local>
2025-06-25 02:04:57 +0900

    feat: docker-compose 수정

--- config/redisClient.ts
+++ config/redisClient.ts
@@ -1,6 +1,11 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+const client = createClient({
+  socket: {
+    host: process.env.REDIS_HOST || 'redis',
+    port: Number(process.env.REDIS_PORT) || 6379,
+  },
+});
 
 client.on('error', err => {
   console.error('Redis Client Error', err);
